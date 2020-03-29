f0c44667a86348366f4d4e4e8cec38e42d403c4b
khine wai oo <khingwai10@gmail.com>
2020-03-29 18:52:30 +0900

    Login Screen Design and Nav Bottom

---
+++ android/app/src/main/kotlin/com/example/safehome/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.safehome
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/ui/washhand.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../common/ui/circle-button.dart';
+import '../styles/AppStyles.dart';
+
+class WashHand extends StatefulWidget {
+  @override
+  State createState() => _State();
+}
+
+class _State extends State<WashHand> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 60,
+        margin: EdgeInsets.symmetric(
+            vertical: nav_margin_vert, horizontal: nave_margin_hor),
+        child: Row(
+          children: <Widget>[
+            Container(
+                width: nav_btn_width,
+                child: CircleButton(
+                  onPressed: () {},
+                  icon: Icons.pan_tool,
+                  iconColor: Colors.white70,
+                  btnColor: Color(0xFFFF6D00),
+                )),
+            Container(
+                width: nav_btn_width,
+                child: CircleButton(
+                  onPressed: () {},
+                  icon: Icons.home,
+                  iconColor: Colors.white70,
+                  btnColor: Color(0xFF4285F4),
+                )),
+            Container(
+                width: nav_btn_width,
+                child: CircleButton(
+                  onPressed: () {},
+                  icon: Icons.group_add,
+                  iconColor: Colors.white70,
+                  btnColor: Color(0xFF4285F4),
+                )),
+            Container(
+              width: nav_btn_width,
+              child: CircleAvatar(
+                  radius: nav_btn_width,
+                  backgroundColor: Colors.blueAccent,
+                  child: CircleButton(
+                    onPressed: () {},
+                    icon: Icons.share,
+                    iconColor: Colors.white70,
+                    btnColor: Color(0xFF4285F4),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
